8e5701934a9fe52bbe5dae279d82c21b26a1875a
quantik-git <machado.david190@gmail.com>
2019-02-02 15:35:47 +0000

    removed spec/

--- spec/language-stormbreaker-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import LanguageStormbreakerView from '../lib/language-stormbreaker-view';
-
-describe('LanguageStormbreakerView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
