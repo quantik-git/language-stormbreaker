55fef4fecaa3e305e01cd9b72cfd9a804204edc2
quantik-git <machado.david190@gmail.com>
2019-02-02 01:54:33 +0000

    bla bla bla

---
+++ spec/language-stormbreaker-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import LanguageStormbreakerView from '../lib/language-stormbreaker-view';
+
+describe('LanguageStormbreakerView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
